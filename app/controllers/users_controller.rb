547a8b33232419215438ee8a4a5d26330a5cf80b
Yuichi Takada <takadyuichi@gmail.com>
2015-08-08 18:19:40 +0900

    init view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show, :new, :create, :update, :destroy]
+  resources :races, only: [:index, :show, :new, :create, :destroy]
+
   namespace :api, constraints: { format: :json } do
     resources :votes, only: [:create, :update], defaults: {format: :json}
     resources :users, only: [:show, :create, :update, :destroy], defaults: {format: :json}
