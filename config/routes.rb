6e3790f974b8eeeb74510327dbdfd8df60c62c4e
Yuichi Takada <takadyuichi@gmail.com>
2015-08-08 18:23:36 +0900

    cosme

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :new, :create, :update, :destroy]
   resources :races, only: [:index, :show, :new, :create, :destroy]
+  resources :users, only: [:show, :new, :create, :update, :destroy]
 
   namespace :api, constraints: { format: :json } do
-    resources :votes, only: [:create, :update], defaults: {format: :json}
-    resources :users, only: [:show, :create, :update, :destroy], defaults: {format: :json}
-    resources :races, only: [:index, :show, :create, :destroy], defaults: {format: :json}
+    resources :races, only: [:index, :show, :create, :destroy]
+    resources :votes, only: [:create, :update]
+    resources :users, only: [:show, :create, :update, :destroy]
   end
 end
