b1f8ab66c5e17b424cffbfc854e2dd6bc7f8da28
Yuichi Takada <takadyuichi@gmail.com>
2015-08-30 15:36:15 +0900

    create logout action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'top#index'
   get '/auth/twitter/callback' => 'sessions#create'
+  get '/logout' => 'sessions#destroy', as: :logout
 
   post '/races/vote' => 'races#vote'
   resources :races, only: [:index, :show, :new, :create, :destroy]
