aafeba516250ee432576f100fdfb17c8381ff24c
Yuichi Takada <takadyuichi@gmail.com>
2015-08-30 15:30:25 +0900

    add description

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,11 @@ class User < ActiveRecord::Base
     nickname = auth_hash[:info][:nickname]
     image_url = auth_hash[:info][:image]
     name = auth_hash[:info][:nickname]
+    description = auth_hash[:info][:description]
 
     User.find_or_create_by(provider: provider, uid: uid) do |user|
       user.name = name
+      user.description = description
       user.nickname = nickname
       user.image_url = image_url
     end
