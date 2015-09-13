12b78cc299660bb76962666807d6f2a870dd497a
Yuichi Takada <takadyuichi@gmail.com>
2015-09-13 23:52:48 +0900

    user can edit description and image_url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :image_url, :description)
     end
 end
