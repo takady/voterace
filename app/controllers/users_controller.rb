f4689aa60514d7e1692e810737fc42515e504929
Yuichi Takada <takadyuichi@gmail.com>
2015-09-13 22:48:46 +0900

    `current_user` instead of `user`, clearly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :current_user
 
   def show
   end
@@ -8,25 +8,20 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+    if @current_user.update(user_params)
+      redirect_to @current_user, notice: 'User was successfully updated.'
     else
       render :edit
     end
   end
 
   def destroy
-    @user.destroy
+    @current_user.destroy
 
     redirect_to users_url, notice: 'User was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email)
