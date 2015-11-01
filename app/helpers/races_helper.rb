90857b74e161f8590e9fe3a377a7dc9ab07226a4
Yuichi Takada <takadyuichi@gmail.com>
2015-11-01 13:49:51 +0900

    refactor: define helper method for vote_button

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -1,2 +1,12 @@
 module RacesHelper
+
+  def vote_button(race, candidate_code)
+    candidate = race.send("candidate_#{candidate_code}")
+    btn_class = candidate_code == 1 ? 'btn btn-danger' : 'btn btn-primary'
+
+    button_label = candidate
+    button_label << ": #{race.votes.count_of_candidate(candidate_code)}" if race.voted_by?(current_user)
+
+    button_to button_label, races_vote_path(vote: { race_id: race.id, candidate: candidate_code }), class: btn_class
+  end
 end
