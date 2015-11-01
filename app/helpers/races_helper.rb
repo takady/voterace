8364cbca0108a4abc32ac70c26fd0d9dda82d154
Yuichi Takada <takadyuichi@gmail.com>
2015-11-01 14:34:18 +0900

    off the button color

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -2,11 +2,11 @@ module RacesHelper
 
   def vote_button(race, candidate_code)
     candidate = race.send("candidate_#{candidate_code}")
-    btn_class = candidate_code == 1 ? 'btn btn-danger' : 'btn btn-primary'
+    button_class = "btn btn-default candidate candidate_#{candidate_code}"
 
     button_label = candidate
     button_label << ": #{race.votes.count_of_candidate(candidate_code)}" if race.voted_by?(current_user)
 
-    button_to button_label, races_vote_path(vote: { race_id: race.id, candidate: candidate_code }), class: btn_class
+    button_to button_label, races_vote_path(vote: { race_id: race.id, candidate: candidate_code }), class: button_class
   end
 end
