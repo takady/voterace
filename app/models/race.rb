10911a79d43968e11a9bbdbde0938eb0a9da6126
Yuichi Takada <takadyuichi@gmail.com>
2015-11-01 12:47:38 +0900

    show the amount of votes

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,6 +1,10 @@
 class Race < ActiveRecord::Base
   belongs_to :user
-  has_many :votes
+  has_many :votes do
+    def count_of_candidate(candidate)
+      where(candidate: candidate).count
+    end
+  end
 
   def voted_by?(user)
     votes.find_by(user_id: user.id)
