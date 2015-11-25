5e7d5f0b9719d54510d4f10e22f7a662fb25f6c7
Yuichi Takada <takadyuichi@gmail.com>
2015-11-26 08:46:02 +0900

    install and setup omniauth-facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'top#index'
   get '/auth/twitter/callback' => 'sessions#create'
+  get '/auth/facebook/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
 
   post '/races/vote' => 'races#vote'
