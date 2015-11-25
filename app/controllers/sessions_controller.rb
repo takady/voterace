df32ecb1c8b6b6163429c7aa6937da92666d4458
Yuichi Takada <takadyuichi@gmail.com>
2015-11-25 23:37:07 +0900

    refactor: use omniauth hash like hashie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   before_action :authenticate, except: [:create, :destroy]
 
   def create
-    user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     session[:user_id] = user.id
     redirect_to root_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,12 @@ class User < ActiveRecord::Base
   has_many :votes
   has_many :created_races, class_name: 'Race', foreign_key: :user_id
 
-  def self.find_or_create_from_auth_hash(auth_hash)
-    provider = auth_hash[:provider]
-    uid = auth_hash[:uid]
-    nickname = auth_hash[:info][:nickname]
-    image_url = auth_hash[:info][:image]
-    name = auth_hash[:info][:nickname]
-    description = auth_hash[:info][:description]
-
-    User.find_or_create_by(provider: provider, uid: uid) do |user|
-      user.name = name
-      user.description = description
-      user.nickname = nickname
-      user.image_url = image_url
+  def self.find_or_create_from_auth(auth)
+    find_or_create_by!(provider: auth.provider, uid: auth.uid) do |user|
+      user.name = auth.info.nickname
+      user.description = auth.info.description
+      user.nickname = auth.info.nickname
+      user.image_url = auth.info.image
     end
   end
 
