696461c8065d10f5cc67a61476fd4edba28dee14
Yuichi Takada <takadyuichi@gmail.com>
2016-01-10 23:27:55 +0900

    user page is also opened for every visitor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate, except: [:new, :create]
+  before_action :authenticate, except: [:show, :new, :create]
   before_action :set_current_user, only: [:edit, :update, :destroy]
 
   def mypage
