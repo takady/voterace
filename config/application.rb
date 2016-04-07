f728170626e37d0569942041933f8bda4f2c02bd
Yuichi Takada <takadyuichi@gmail.com>
2016-04-07 17:26:32 +0900

    config timezone

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,8 @@ Bundler.require(*Rails.groups)
 
 module Voterace
   class Application < Rails::Application
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
+    config.time_zone = 'UTC'
+    config.active_record.default_timezone = :utc
 
     config.generators do |generator|
       generator.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
