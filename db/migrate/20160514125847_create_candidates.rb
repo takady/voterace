2784a56f1b8f53d836d01fe4bde8c4ec0f642d74
Yuichi Takada <takadyuichi@gmail.com>
2016-05-15 00:07:22 +0900

    Create Candidate model

---
+++ db/migrate/20160514125847_create_candidates.rb
@@ -0,0 +1,12 @@
+class CreateCandidates < ActiveRecord::Migration
+  def change
+    create_table :candidates do |t|
+      t.references :race, index: true, foreign_key: true
+      t.string :name, null: false
+      t.integer :order, nill: false
+      t.integer :votes_count, default: 0
+
+      t.timestamps null: false
+    end
+  end
+end
