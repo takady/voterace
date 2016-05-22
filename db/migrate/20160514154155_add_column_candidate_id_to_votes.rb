ed6ee2892c88cc7968592b9dbba53d24b555b1f8
Yuichi Takada <takadyuichi@gmail.com>
2016-05-22 16:03:38 +0900

    Remove data insert

--- db/migrate/20160514154155_add_column_candidate_id_to_votes.rb
+++ db/migrate/20160514154155_add_column_candidate_id_to_votes.rb
@@ -2,10 +2,6 @@ class AddColumnCandidateIdToVotes < ActiveRecord::Migration
   def up
     rename_column :votes, :candidate, :candidate_order
     add_column :votes, :candidate_id, :integer, null: false
-
-    Vote.all.each do |vote|
-      vote.update!(candidate_id: Candidate.find_by(race_id: vote.race_id, order: vote.candidate_order).id)
-    end
   end
 
   def down
