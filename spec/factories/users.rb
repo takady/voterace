a84f8a23902323579ee5d2dc808a35c5e697e87b
Yuichi Takada <takadyuichi@gmail.com>
2016-06-08 21:13:10 +0900

    Tweak

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,7 @@ FactoryGirl.define do
     username 'test_user'
     fullname 'Test User'
     email 'test@example.com'
+    image_url 'http://pbs.twimg.com/profile_images/438644586575974401/a-mcG_Nw.jpeg'
+    description 'this is test.'
   end
 end
