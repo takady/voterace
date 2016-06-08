26f5d7bea3968ba681be886178e0a3d0360e2800
Yuichi Takada <takadyuichi@gmail.com>
2016-06-08 21:03:53 +0900

    Change column type

---
+++ db/migrate/20160608115730_change_column_type_description_of_users.rb
@@ -0,0 +1,9 @@
+class ChangeColumnTypeDescriptionOfUsers < ActiveRecord::Migration
+  def up
+    change_column :users, :description, :text
+  end
+
+  def down
+    change_column :users, :description, :string
+  end
+end
