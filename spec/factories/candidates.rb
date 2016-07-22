f9a17470ce405fb082b36730033e457074ecdd89
Yuichi Takada <takadyuichi@gmail.com>
2016-07-23 08:41:57 +0900

    Fix column names

--- spec/factories/candidates.rb
+++ spec/factories/candidates.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
     race_id 1
     name "MyString"
     order 1
-    votes_count 1
   end
 
 end
--- spec/factories/races.rb
+++ spec/factories/races.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :race do
     title 'Which color do you like?'
-    candidate_1 'red'
-    candidate_2 'blue'
   end
 end
