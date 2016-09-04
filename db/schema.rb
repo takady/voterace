43564c4f918bf5637de82bf325c51c7f7b5ca5ca
Yuichi Takada <takadyuichi@gmail.com>
2016-09-04 12:33:56 +0900

    Exec rake db:migrate with rails 5

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,61 +12,56 @@
 
 ActiveRecord::Schema.define(version: 20160608115730) do
 
-  create_table "candidates", force: :cascade do |t|
-    t.integer  "race_id",    limit: 4
-    t.string   "name",       limit: 255, null: false
-    t.integer  "order",      limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+  create_table "candidates", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "race_id"
+    t.string   "name",       null: false
+    t.integer  "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["race_id"], name: "index_candidates_on_race_id", using: :btree
   end
 
-  add_index "candidates", ["race_id"], name: "index_candidates_on_race_id", using: :btree
-
-  create_table "races", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.string   "title",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.datetime "expired_at",             null: false
+  create_table "races", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "expired_at", null: false
+    t.index ["user_id"], name: "index_races_on_user_id", using: :btree
   end
 
-  add_index "races", ["user_id"], name: "index_races_on_user_id", using: :btree
-
-  create_table "social_profiles", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.string   "provider",   limit: 255
-    t.string   "uid",        limit: 255
-    t.string   "username",   limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+  create_table "social_profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_social_profiles_on_provider_and_uid", unique: true, using: :btree
+    t.index ["user_id"], name: "index_social_profiles_on_user_id", using: :btree
   end
 
-  add_index "social_profiles", ["provider", "uid"], name: "index_social_profiles_on_provider_and_uid", unique: true, using: :btree
-  add_index "social_profiles", ["user_id"], name: "index_social_profiles_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username",    limit: 255,   null: false
-    t.string   "email",       limit: 255
-    t.string   "fullname",    limit: 255
-    t.string   "image_url",   limit: 255
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "username",                  null: false
+    t.string   "email"
+    t.string   "fullname"
+    t.string   "image_url"
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
-
-  create_table "votes", force: :cascade do |t|
-    t.integer  "user_id",      limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "candidate_id", limit: 4, null: false
-    t.integer  "race_id",      limit: 4
+  create_table "votes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "race_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "candidate_id", null: false
+    t.index ["candidate_id"], name: "index_votes_on_candidate_id", using: :btree
+    t.index ["race_id"], name: "index_votes_on_race_id", using: :btree
+    t.index ["user_id"], name: "index_votes_on_user_id", using: :btree
   end
-
-  add_index "votes", ["candidate_id"], name: "index_votes_on_candidate_id", using: :btree
-  add_index "votes", ["race_id"], name: "index_votes_on_race_id", using: :btree
-  add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
 
 end
