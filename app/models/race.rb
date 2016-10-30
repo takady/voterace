923689d16612f69d27f3befb157d64b80c9ab5b8
Yuichi Takada <takadyuichi@gmail.com>
2016-10-30 15:38:58 +0900

    Extract calculate method to a concern

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,4 +1,6 @@
 class Race < ApplicationRecord
+  include ::Concerns::PercentageCalculable
+
   belongs_to :user
   has_many :candidates, dependent: :destroy
   accepts_nested_attributes_for :candidates
@@ -30,12 +32,10 @@ class Race < ApplicationRecord
   end
 
   def vote_rates
-    votes_for_1 = candidates.find_by(order: 1).votes.count
-    votes_for_2 = candidates.find_by(order: 2).votes.count
-    total_votes = votes_for_1 + votes_for_2
-    vote_rate_of_1 = ((votes_for_1/total_votes.to_f) * 100).round
-    vote_rate_of_2 = ((votes_for_2/total_votes.to_f) * 100).round
+    votes_of_each_candidate = Candidate::ORDERS.map {|order|
+      send("candidate_#{order}").votes.count
+    }
 
-    [vote_rate_of_1, vote_rate_of_2]
+    percents_of(votes_of_each_candidate)
   end
 end
