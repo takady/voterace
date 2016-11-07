9bfc3b60b590bd8f4974ce67aacb1541f6faa0eb
Yuichi Takada <takadyuichi@gmail.com>
2016-11-07 23:59:56 +0900

    Create CandidateWithChart react component

---
+++ app/assets/javascripts/components/candidate_with_chart.js.jsx
@@ -0,0 +1,40 @@
+var CandidateWithChart = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    order: React.PropTypes.number.isRequired,
+    voteUrl: React.PropTypes.string.isRequired,
+    voteRate: React.PropTypes.number.isRequired,
+    mostVoted: React.PropTypes.bool
+  },
+
+  render: function() {
+    let chart;
+
+    if (this.props.mostVoted) {
+      chart = (
+        <div className='candidate-chart most-voted' style={{width : this.props.voteRate + '%'}}>
+          <b className='vote-rate'>{this.props.voteRate}%</b>
+          {this.props.name}
+          <i className='voted fa fa-check-circle-o'></i>
+        </div>
+      )
+    } else {
+      chart = (
+        <div className='candidate-chart' style={{width : this.props.voteRate + '%'}}>
+          <b className='vote-rate'>{this.props.voteRate}%</b>
+          <b>
+            <a data-toggle='tooltip' data-placement='right' rel='nofollow' data-method='post' href={this.props.voteUrl} data-original-title='vote!'>
+              {this.props.name}
+            </a>
+          </b>
+        </div>
+      )
+    }
+
+    return (
+      <div className='candidate'>
+        { chart }
+      </div>
+    );
+  }
+});
