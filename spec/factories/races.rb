77488069703e203ada1048f19e395273c7524b87
Yuichi Takada <takadyuichi@gmail.com>
2017-01-22 00:05:17 +0900

    FactoryGirl create race with candidates by default

--- spec/factories/races.rb
+++ spec/factories/races.rb
@@ -1,5 +1,20 @@
 FactoryGirl.define do
   factory :race do
     title 'Which color do you like?'
+    expired_at Time.current.since(1.day)
+
+    with_candidates
+
+    trait :with_candidates do
+      transient do
+        number_of_candidates 2
+      end
+
+      after(:build) do |race, evaluator|
+        evaluator.number_of_candidates.times.with_index(1) do |index|
+          race.candidates.build(name: 'one', order: index)
+        end
+      end
+    end
   end
 end
