6581c3a297b608e7e12fed8f5f390557831cf73e
Yuichi Takada <takadyuichi@gmail.com>
2017-02-04 16:09:20 +0900

    Change routing

--- app/controllers/api/votes_controller.rb
+++ app/controllers/api/votes_controller.rb
@@ -1,15 +1,11 @@
 class Api::VotesController < Api::ApiController
   def create
-    race = Race.votable.find_by(params[:race_id])
+    candidate = Candidate.find_by(id: params[:candidate_id])
+
+    head :bad_request and return unless candidate.votable?
 
     current_user.vote_for(candidate)
 
-    render json: Resource::Race.new(race, current_user: current_user).to_response
-  end
-
-  private
-
-  def candidate
-    Candidate.find_by!(race_id: params[:race_id], order: params[:candidate_order])
+    render json: Resource::Race.new(candidate.race, current_user: current_user).to_response
   end
 end
