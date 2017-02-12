e6f495b3eb679c5c6d5583ae9273c7b436281096
Yuichi Takada <takadyuichi@gmail.com>
2017-02-12 11:56:36 +0900

    Remove calculate logic from race model
    Currently, calculate in js

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,6 +1,4 @@
 class Race < ApplicationRecord
-  include ::Concerns::PercentageCalculable
-
   DEFAULT_LIFETIME = 30.days
   REQUIRED_NUMBER_OF_CANDIDATES = 2
 
@@ -51,16 +49,6 @@ class Race < ApplicationRecord
 
   def votable?(at: Time.zone.now)
     expired_at > at
-  end
-
-  def vote_rates_of_each_candidate
-    votes_of_each_candidate = candidates.order(:order).map {|candidate|
-      candidate.votes.count
-    }
-
-    percents_of(votes_of_each_candidate).each_with_index.each_with_object({}) {|(percent, index), result|
-      result[index+1] = percent
-    }
   end
 
   def most_voted_candidate
