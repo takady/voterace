85112cb7b9a22ce98bd865187ada02158258f8c5
Yuichi Takada <takadyuichi@gmail.com>
2017-02-19 22:36:57 +0900

    Cache node_modules

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,16 @@ set :log_level, :info
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push(
+  'log',
+  'tmp/pids',
+  'tmp/cache',
+  'tmp/sockets',
+  'vendor/bundle',
+  'public/system',
+  'node_modules',
+  'client/node_modules'
+)
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
