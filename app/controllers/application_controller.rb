bdbc217a0573d930b539b3623b4ec873934d66b9
Yuichi Takada <takadyuichi@gmail.com>
2017-05-05 11:24:34 +0900

    protect from forgery except rendering error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, except: [:render_404, :render_500]
 
   before_action :authenticate, except: [:render_404, :render_500]
 
