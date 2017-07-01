36a3e838998c7917465812025cb398950f058d35
Yuichi Takada <takadyuichi@gmail.com>
2017-07-01 13:53:59 +0900

    Includes associate models

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -1,16 +1,16 @@
 class Api::RacesController < Api::ApiController
   before_action :authenticate, except: [:index, :show]
-  before_action :set_race, only: [:show, :destroy]
 
   def index
     # TODO make it possible to fetch by cursor
-    races = Race.votable.order('id DESC').first(30)
+    races = Race.includes(candidates: :votes).votable.order('id DESC').first(30)
 
     render json: races.map {|race| Resource::Race.new(race, current_user: current_user).to_response }
   end
 
   def show
-    render json: Resource::Race.new(@race, current_user: current_user).to_response
+    race = Race.includes(candidates: :votes).find(params[:id])
+    render json: Resource::Race.new(race, current_user: current_user).to_response
   end
 
   def create
@@ -24,18 +24,16 @@ class Api::RacesController < Api::ApiController
   end
 
   def destroy
-    head :bad_request and return unless current_user.id == @race.user_id
+    race = Race.find(params[:id])
 
-    @race.destroy!
+    head :bad_request and return unless current_user.id == race.user_id
+
+    race.destroy!
 
     head :ok
   end
 
   private
-
-  def set_race
-    @race = Race.find(params[:id])
-  end
 
   def create_params
     params.permit(:title, :expired_at, candidates: [])
