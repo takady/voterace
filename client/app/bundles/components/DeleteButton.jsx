8eed3eb15ec945a8ba962afff10c007150663c91
Yuichi Takada <takadyuichi@gmail.com>
2017-07-23 18:58:40 +0900

    Delete race by api

--- client/app/bundles/components/DeleteButton.jsx
+++ client/app/bundles/components/DeleteButton.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -11,15 +12,40 @@ export default class DeleteButton extends React.Component {
     this.state = {data: null};
   }
 
+  deleteRace() {
+    axios.delete(`/api/races/${this.props.id}`, {
+      withCredentials: true,
+      headers: {'X-CSRF-TOKEN': ReactOnRails.authenticityToken()}
+    })
+      .then(() => {
+        window.location.href = '/';
+      })
+      .catch(error => {
+        switch (error.response.status) {
+        case 401:
+          window.location.href = '/signin';
+          break;
+        case 404:
+          window.location.href = '/404.html';
+          break;
+        case 500:
+          window.location.href = '/500.html';
+          break;
+        default:
+          window.location.href = '/500.html';
+          console.error(error);
+          break;
+        }
+      });
+  }
+
   render() {
     return (
       <p>
         <a
           data-confirm="Are you sure?"
           className="btn btn-danger btn-destroy"
-          rel="nofollow"
-          data-method="delete"
-          href={'/races/' + this.props.id}
+          onClick={() => this.deleteRace()}
         >
           <i className="fa fa-trash-o"></i> Delete this race
         </a>
